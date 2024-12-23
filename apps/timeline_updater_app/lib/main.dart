50a32ae6645d0c5fd832946daadebcb35da4234c
Florian Leeser <fleeser@coderave.dev>
2024-12-23 17:26:18 +0100

    chore: add Supabase config

--- apps/timeline_updater_app/lib/main.dart
+++ apps/timeline_updater_app/lib/main.dart
@@ -1,20 +1,37 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import 'package:timeline_updater_app/core/utils/env.dart';
+import 'package:timeline_updater_app/src/app.dart';
+
+/// The main entry point for the application
+Future<void> main() async {
+
+  /// Ensure that the widgets binding is initialized
+  WidgetsFlutterBinding.ensureInitialized();
+
+  /// Load the environment variables
+  await loadEnv();
+
+  /// Initialize the Supabase client
+  await initializeSupabase();
+
+  /// Run the application
+  runApp(const App());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+/// Loads the environment variables
+Future<void> loadEnv() async {
+  await dotenv.load();
+}
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+/// Initializes the Supabase client
+Future<void> initializeSupabase() async {
+  await Supabase.initialize(
+    url: Env.supabaseUrl,
+    anonKey: Env.supabaseAnonKey,
+    authOptions: const FlutterAuthClientOptions(detectSessionInUri: false)
+  );
 }
---
+++ apps/timeline_updater_customer_app/lib/src/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+/// The main application widget
+class App extends StatelessWidget {
+
+  /// Default constructor
+  const App({ super.key });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
