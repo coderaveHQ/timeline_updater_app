997a8a9700a6d2431ed609f3366db2f19fe2f61b
Florian Leeser <fleeser@coderave.dev>
2025-01-02 16:52:22 +0100

    chore: refactor initialization

--- apps/timeline_updater_app/lib/main.dart
+++ apps/timeline_updater_app/lib/main.dart
@@ -1,71 +1,13 @@
-import 'dart:io';
+import 'package:timeline_updater_app_utils/timeline_updater_app_utils.dart';
 
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:bitsdojo_window/bitsdojo_window.dart';
-
-import 'package:timeline_updater_app/core/utils/env.dart';
-import 'package:timeline_updater_app/src/app.dart';
 import 'package:timeline_updater_app/firebase_options.dart';
+import 'package:timeline_updater_app/src/app.dart';
 
 /// The main entry point for the application
 Future<void> main() async {
-
-  // Ensure that the widgets binding is initialized
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // Load the environment variables
-  await loadEnv();
-
-  // Initialize the Supabase client
-  await initializeSupabase();
-
-  // Initialize the Firebase client
-  await initializeFirebase();
-
-  // Run the application
-  runApp(const App());
-
-  // Configure the window
-  configureDesktopWindow();
-}
-
-/// Loads the environment variables
-Future<void> loadEnv() async {
-  await dotenv.load(fileName: 'dotenv');
-}
-
-/// Initializes the Supabase client
-Future<void> initializeSupabase() async {
-  await Supabase.initialize(
-    url: Env.supabaseUrl,
-    anonKey: Env.supabaseAnonKey,
-    authOptions: const FlutterAuthClientOptions(detectSessionInUri: false)
+  // Initialize the app and run it
+  await initializeAndRun(
+    app: const App(), 
+    firebaseOptions: DefaultFirebaseOptions.currentPlatform
   );
-}
-
-/// Initializes the Firebase client
-Future<void> initializeFirebase() async {
-  // Skip when platform is Linux since Firebase does not support this platform, yet
-  if (!kIsWeb && Platform.isLinux) return;
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-}
-
-/// Configures the desktop window
-void configureDesktopWindow() {
-  // Skip when platform is Web or Mobile since there is no window to be configured
-  if (kIsWeb || Platform.isAndroid || Platform.isIOS) return;
-
-  // Configure the window
-  doWhenWindowReady(() {
-    const Size initialSize = Size(600.0, 450.0);
-    appWindow.minSize = initialSize;
-    appWindow.size = initialSize;
-    appWindow.alignment = Alignment.center;
-    appWindow.show();
-  });
 }
---
+++ packages/timeline_updater_app_utils/lib/initialization.dart
@@ -0,0 +1,82 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:bitsdojo_window/bitsdojo_window.dart';
+
+/// A class that provides access to environment variables
+abstract class Env {
+
+  /// A getter for the Supabase URL
+  static String get supabaseUrl => getKey('SUPABASE_URL');
+
+  /// A getter for the Supabase anonymous key
+  static String get supabaseAnonKey => getKey('SUPABASE_ANON_KEY');
+
+  /// Helper method to get the value of an environment variable
+  static String getKey(String key) {
+    return dotenv.env[key]!;
+  }
+}
+
+/// Initializes and runs the App
+Future<void> initializeAndRun({ 
+  required Widget app,
+  required FirebaseOptions firebaseOptions 
+}) async {
+  // Ensure that the widgets binding is initialized
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Load the environment variables
+  await _loadEnv();
+
+  // Initialize the Supabase client
+  await _initializeSupabase();
+
+  // Initialize the Firebase client
+  await _initializeFirebase(firebaseOptions);
+
+  // Run the application
+  runApp(app);
+
+  // Configure the window
+  _configureDesktopWindow();
+}
+
+/// Loads the environment variables
+Future<void> _loadEnv() async {
+  await dotenv.load(fileName: 'dotenv');
+}
+
+/// Initializes the Supabase client
+Future<void> _initializeSupabase() async {
+  await Supabase.initialize(
+    url: Env.supabaseUrl,
+    anonKey: Env.supabaseAnonKey,
+    authOptions: const FlutterAuthClientOptions(detectSessionInUri: false)
+  );
+}
+
+/// Initializes the Firebase client
+Future<void> _initializeFirebase(FirebaseOptions options) async {
+  // Skip when platform is Linux since Firebase does not support this platform, yet
+  if (!kIsWeb && Platform.isLinux) return;
+  await Firebase.initializeApp(options: options);
+}
+
+/// Configures the desktop window
+void _configureDesktopWindow() {
+  // Skip when platform is Web or Mobile since there is no window to be configured
+  if (kIsWeb || Platform.isAndroid || Platform.isIOS) return;
+  doWhenWindowReady(() {
+    const Size initialSize = Size(600.0, 450.0);
+    appWindow.minSize = initialSize;
+    appWindow.size = initialSize;
+    appWindow.alignment = Alignment.center;
+    appWindow.show();
+  });
+}
